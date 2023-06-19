4085375684ef67431ae2c6a14795b3408db27c7c
Muhammad Atif Pervaiz <matifpervaizofficial@gmail.com>
2023-06-19 14:31:45 +0500

    login api and verify otp

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:holidays/screens/companyauth/companyLogin.dart';
 import 'package:holidays/screens/dashboard.dart';
+
 import 'package:holidays/screens/home.dart';
-import 'package:holidays/screens/splashscreen.dart';
-import 'package:holidays/screens/testingscreen.dart';
+import 'package:holidays/screens/companyauth/forgotpass.dart';
 import 'package:holidays/screens/userauth/forgotpass.dart';
 import 'package:holidays/screens/userauth/login.dart';
-import 'package:holidays/screens/userauth/profile.dart';
 import 'package:holidays/screens/userauth/signup.dart';
 import 'package:google_fonts/google_fonts.dart';
 // ...
+import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:holidays/tst.dart';
+import 'package:holidays/viewmodel/company/compuserviewmodel.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // var directory = await getApplicationDocumentsDirectory();
+  //   Hive.init(directory.path);
+  await Hive.initFlutter();
+  await Hive.openBox('box');
 
   runApp(const MyApp());
 }
@@ -24,16 +32,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
-      home: LeaveScreen(),
-      routes: {
-        ForgitPassword.idScreen: (context) => ForgitPassword(),
-        LoginPage.routeName: (context) => LoginPage(),
-        SignupPage.idScreen: (context) => SignupPage(),
-        HomePage.routeName: (context) => HomePage()
-      },
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => CompanyViewModel(),
+          )
+        ],
+        child: MaterialApp(
+          builder: EasyLoading.init(),
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            fontFamily: GoogleFonts.poppins().fontFamily,
+          ),
+          home: CompanyLoginPage(),
+          routes: {
+            EmpForgitPassword.idScreen: (context) => EmpForgitPassword(),
+            EmpLoginPage.routeName: (context) => EmpLoginPage(),
+            SignupPage.idScreen: (context) => SignupPage(),
+            HomePage.routeName: (context) => HomePage()
+          },
+        ));
   }
 }
---
+++ lib/viewmodel/company/compuserviewmodel.dart
@@ -0,0 +1,94 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:holidays/screens/companyauth/profile.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../../models/company/compusermodel.dart';
+import '../../screens/companyauth/otp.dart';
+import '../../widget/popuploader.dart';
+
+class CompanyViewModel extends ChangeNotifier {
+  CompanyUser? _user;
+  String? _token;
+
+  CompanyUser? get user => _user;
+  String? get token => _token;
+
+  Future<void> performLogin(
+      String email, String password, BuildContext context) async {
+    final String apiUrl = 'https://jporter.ezeelogix.com/public/api/login';
+    PopupLoader.show();
+
+    final response = await http.post(Uri.parse(apiUrl),
+        body: {'email': email, 'password': password, 'user_type': '1'});
+    PopupLoader.hide();
+
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+
+      if (jsonData['status'] == 'Success') {
+        final userJson = jsonData['data']['user'];
+        final token = jsonData['data']['token'];
+
+        _user = CompanyUser.fromJson(userJson);
+        _token = token;
+
+        // Store data in shared preferences
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        prefs.setString('token', _token!);
+        // Serialize and store user object if needed
+        print(jsonData);
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => CompanyProfileView()),
+        );
+        notifyListeners();
+      } else {
+        // Login failed
+        Fluttertoast.showToast(
+            msg: "Verify your email through OTP sent to your email",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => CompanyOTPScreen()),
+        );
+        print('');
+
+        print('Login failed');
+      }
+    } else {
+      // Error occurred
+      print('Error: ${response.reasonPhrase}');
+    }
+  }
+
+  Future<void> retrieveUserDataFromSharedPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _token = prefs.getString('token');
+    // Retrieve and deserialize user object if stored
+
+    // Notify listeners about the updated data
+    notifyListeners();
+  }
+
+  Future<void> signOut() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('token');
+    // Clear any other stored user data if needed
+
+    _user = null;
+    _token = null;
+
+    // Notify listeners about the updated data
+    notifyListeners();
+  }
+}
